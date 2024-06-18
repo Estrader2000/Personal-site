94685883ce52a0fa9e028c291d1d458781eab2c0
Ely-Strader <elystrader1@gmail.com>
2024-06-18 03:29:48 -0700

    uninstalled react-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,40 +1,11 @@
 import { ThemeProvider } from "@/app/theme-provider";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./RootLayout";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Projects from "./routes/Projects";
-import Contact from "./routes/Contact";
-
-const router = createBrowserRouter([
-  {
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/#home",
-        element: <Home />,
-      },
-      {
-        path: "/#about",
-        element: <About />,
-      },
-      {
-        path: "/#projects",
-        element: <Projects />,
-      },
-      {
-        path: "/#contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <RouterProvider router={router} />
+        <RootLayout />
       </ThemeProvider>
     </>
   );
